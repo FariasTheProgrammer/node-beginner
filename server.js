f3502fdf90fca16305b8e0b4bcea5d6be73e957c
FariasTheProgrammer <fariarodgabriel@gmail.com>
2025-02-24 10:44:17 -0300

    Fastify

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) => {
-    response.write('oi') 
+// const server = createServer((request, response) => {
+//     response.write('oi') 
         
-    return response.end()
+//     return response.end()
     
+// })
+
+// server.listen(3333)
+
+// SERVER WITH MICRO-FRAMEWORK FASTIFY
+
+import { fastify } from 'fastify'
+
+const server = fastify()
+
+server.get('/', () =>{
+    return 'Hello World'
 })
 
-server.listen(3333)
+
+
+server.get('/alestis', () =>{
+    return 'Hello Alestis'
+})
+
+server.get('/aciturri', () =>{
+    return 'Hello Aciturri'
+})
+
+server.listen({
+    port: 3333,
+})
